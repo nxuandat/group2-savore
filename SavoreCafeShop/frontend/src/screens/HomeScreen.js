8c2d538f2deb4dde00105e80676046edab2e7138
DinziloZ <121272808+DinziloZ@users.noreply.github.com>
2023-07-18 09:20:19 +0700

    fetch products from backend and push to frontend
    Terminal installing:
    Backend:
    1) npm install nodemon --save-dev
    
    Frontend:
    1) npm install axios
    
    To run a website:
    npm start (in Backend) + npm start (in Frontend)

--- SavoreCafeShop/frontend/src/screens/HomeScreen.js
+++ SavoreCafeShop/frontend/src/screens/HomeScreen.js
@@ -1,16 +1,28 @@
-import data from "../data";
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+// import data from '../data';
 
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h1>Featured Products</h1>
-      <div className='products'>
-        {data.products.map((product) => (
-          <div className='product' key={product.slug}>
+      <div className="products">
+        {products.map((product) => (
+          <div className="product" key={product.slug}>
             <a href={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
             </a>
-            <div className='product-info'>
+            <div className="product-info">
               <a href={`/product/${product.slug}`}>
                 <p>{product.name}</p>
               </a>
