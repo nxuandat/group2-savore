a05aa4f790d83560234b9e968729c629bc3f8363
DinziloZ <121272808+DinziloZ@users.noreply.github.com>
2023-07-22 20:53:10 +0700

    Complete Cart Screen
    - No terminal installing
    - Run website normally by:
    + npm start (in Backend terminal) then npm start (in Frontend terminal)

--- SavoreCafeShop/frontend/src/Store.js
+++ SavoreCafeShop/frontend/src/Store.js
@@ -1,16 +1,18 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer } from 'react';
 
 export const Store = createContext();
 
 const initialState = {
   cart: {
-    cartItems: [],
+    cartItems: localStorage.getItem('cartItems')
+      ? JSON.parse(localStorage.getItem('cartItems'))
+      : [],
   },
 };
 
 function reducer(state, action) {
   switch (action.type) {
-    case "CART_ADD_ITEM":
+    case 'CART_ADD_ITEM':
       const newItem = action.payload;
       const existItem = state.cart.cartItems.find(
         (item) => item._id === newItem._id
@@ -20,8 +22,16 @@ function reducer(state, action) {
             item._id === existItem._id ? newItem : item
           )
         : [...state.cart.cartItems, newItem];
+      localStorage.setItem('cartItems', JSON.stringify(cartItems));
       return { ...state, cart: { ...state.cart, cartItems } };
 
+    case 'CART_REMOVE_ITEM': {
+      const cartItems = state.cart.cartItems.filter(
+        (item) => item._id !== action.payload._id
+      );
+      localStorage.setItem('cartItems', JSON.stringify(cartItems));
+      return { ...state, cart: { ...state.cart, cartItems } };
+    }
     default:
       return state;
   }
