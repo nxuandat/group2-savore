5665f5a75984cc861429e6056e40419bba76523e
tranminhchau03 <136145117+tranminhchau03@users.noreply.github.com>
2023-07-22 15:19:20 +0700

    create components
    create a function to return the error message in backend & create 2 components to show a spiner & show the message to the user

--- SavoreCafeShop/frontend/src/screens/ProductScreen.js
+++ SavoreCafeShop/frontend/src/screens/ProductScreen.js
@@ -9,6 +9,9 @@ import Card from "react-bootstrap/Card";
 import Rating from "../components/Rating";
 import Button from "react-bootstrap/Button";
 import { Helmet } from "react-helmet-async";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { getError } from "../utils";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -39,15 +42,15 @@ function ProductScreen() {
         const result = await axios.get(`/api/products/slug/${slug}`);
         dispatch({ type: "FETCH_SUCCESS", payload: result.data });
       } catch (err) {
-        dispatch({ type: "FETCH_FAIL", payload: err.message });
+        dispatch({ type: "FETCH_FAIL", payload: getError(err) });
       }
     };
     fetchData();
   }, [slug]);
   return loading ? (
-    <div>Loanding...</div>
+    <LoadingBox />
   ) : error ? (
-    <div>{error}</div>
+    <MessageBox variant='danger'>{error}</MessageBox>
   ) : (
     <div>
       <Row>
