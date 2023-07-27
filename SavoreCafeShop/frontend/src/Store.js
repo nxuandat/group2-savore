4f0a493292c0344b4d15cffc7289f00eb7b36d2d
YenPhamNhu <119447095+YenPhamNhu@users.noreply.github.com>
2023-07-27 13:16:59 +0700

    Revert "Complete Cart Screen"
    This reverts commit a05aa4f790d83560234b9e968729c629bc3f8363.

--- SavoreCafeShop/frontend/src/Store.js
+++ SavoreCafeShop/frontend/src/Store.js
@@ -1,18 +1,16 @@
-import { createContext, useReducer } from 'react';
+import { createContext, useReducer } from "react";
 
 export const Store = createContext();
 
 const initialState = {
   cart: {
-    cartItems: localStorage.getItem('cartItems')
-      ? JSON.parse(localStorage.getItem('cartItems'))
-      : [],
+    cartItems: [],
   },
 };
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'CART_ADD_ITEM':
+    case "CART_ADD_ITEM":
       const newItem = action.payload;
       const existItem = state.cart.cartItems.find(
         (item) => item._id === newItem._id
@@ -22,16 +20,8 @@ function reducer(state, action) {
             item._id === existItem._id ? newItem : item
           )
         : [...state.cart.cartItems, newItem];
-      localStorage.setItem('cartItems', JSON.stringify(cartItems));
       return { ...state, cart: { ...state.cart, cartItems } };
 
-    case 'CART_REMOVE_ITEM': {
-      const cartItems = state.cart.cartItems.filter(
-        (item) => item._id !== action.payload._id
-      );
-      localStorage.setItem('cartItems', JSON.stringify(cartItems));
-      return { ...state, cart: { ...state.cart, cartItems } };
-    }
     default:
       return state;
   }
--- SavoreCafeShop/frontend/src/screens/CartScreen.js
+++ SavoreCafeShop/frontend/src/screens/CartScreen.js
@@ -7,34 +7,13 @@ import MessageBox from '../components/MessageBox';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default function CartScreen() {
-  const navigate = useNavigate();
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const {
     cart: { cartItems },
   } = state;
-
-  const updateCartHandler = async (item, quantity) => {
-    const { data } = await axios.get(`/api/products/${item._id}`);
-    if (data.countInStock < quantity) {
-      window.alert('Sorry. Product is out of stock');
-      return;
-    }
-    ctxDispatch({
-      type: 'CART_ADD_ITEM',
-      payload: { ...item, quantity },
-    });
-  };
-  const removeItemHandler = (item) => {
-    ctxDispatch({ type: 'CART_REMOVE_ITEM', payload: item });
-  };
-
-  const checkoutHandler = () => {
-    navigate('/signin?redirect=/shipping');
-  };
 
   return (
     <div>
@@ -62,21 +41,12 @@ export default function CartScreen() {
                       <Link to={`/product/${item.slug}`}>{item.name}</Link>
                     </Col>
                     <Col md={3}>
-                      <Button
-                        onClick={() =>
-                          updateCartHandler(item, item.quantity - 1)
-                        }
-                        variant="light"
-                        disabled={item.quantity === 1}
-                      >
+                      <Button variant="light" disabled={item.quantity === 1}>
                         <i className="fas fa-minus-circle"></i>
                       </Button>{' '}
                       <span>{item.quantity}</span>{' '}
                       <Button
                         variant="light"
-                        onClick={() =>
-                          updateCartHandler(item, item.quantity + 1)
-                        }
                         disabled={item.quantity === item.countInStock}
                       >
                         <i className="fas fa-plus-circle"></i>
@@ -84,10 +54,7 @@ export default function CartScreen() {
                     </Col>
                     <Col md={3}>${item.price}</Col>
                     <Col md={2}>
-                      <Button
-                        onClick={() => removeItemHandler(item)}
-                        variant="light"
-                      >
+                      <Button variant="light">
                         <i className="fas fa-trash"></i>
                       </Button>
                     </Col>
@@ -113,7 +80,6 @@ export default function CartScreen() {
                     <Button
                       type="button"
                       variant="primary"
-                      onClick={checkoutHandler}
                       disabled={cartItems.length === 0}
                     >
                       Proceed to Checkout
