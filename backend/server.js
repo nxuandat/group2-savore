b612ef22366a7ff3cb72fc1f947484f215f99cfa
han-1808 <49582098+han-1808@users.noreply.github.com>
2023-07-29 21:53:04 +0700

    Create Sign-in API
    use postman to test at post: http://localhost:5000/api/users/signin
    back-end:
    1. npm install express-async-handler
    2. npm install jsonwebtoken

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
-import productRouter from "./routes/productRoutes.js";
+import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -17,8 +18,17 @@ mongoose
   });
 
 const app = express();
-app.use('/api/seed',seedRouter);
-app.use("/api/products", productRouter);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 // Tạo port
 const port = process.env.PORT || 5000;
