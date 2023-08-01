9e3750be9061c8cf5ef40aa85a1b022b0dfc597b
han-1808 <49582098+han-1808@users.noreply.github.com>
2023-08-01 18:30:27 +0700

    create Paypal [Error]
    client_id: AdPS8jsKfJZDtGyTCi4IpDRaSqshSkzwFY89QOR-tN79Nk76POo-eEqeikPxtq0yDoeSNHm3fcnofPlx
    or
    AatV_mzofT9FZkHYkHTKm47owsVl-KcZL2gCEpO4OUtuTiLRrZ1_YaCcMcVURLwobjXXlyTm_Hfkl1yS
    front-end:
    npm install @paypal/react-paypal-js (--force)

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Pay-Pal Order
+app.get('/api/keys/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
