3f3979a7b6752d3325ed5086ad8f4aa56c763c54
tranminhchau03 <136145117+tranminhchau03@users.noreply.github.com>
2023-08-07 15:31:24 +0700

    Email order receipt by mailgun
    backend install: npm i mailgun-js

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,7 +3,7 @@ import expressAsyncHandler from 'express-async-handler';
 import Order from '../models/orderModel.js';
 import User from '../models/userModel.js';
 import Product from '../models/productModel.js';
-import { isAuth, isAdmin } from '../utils.js';
+import { isAuth, isAdmin, mailgun, payOrderEmailTemplate } from '../utils.js';
 
 const orderRouter = express.Router();
 
@@ -127,7 +127,10 @@ orderRouter.put(
   '/:id/pay',
   isAuth,
   expressAsyncHandler(async (req, res) => {
-    const order = await Order.findById(req.params.id);
+    const order = await Order.findById(req.params.id).populate(
+      'user',
+      'email name'
+    );
     if (order) {
       order.isPaid = true;
       order.paidAt = Date.now();
@@ -139,6 +142,26 @@ orderRouter.put(
       };
 
       const updatedOrder = await order.save();
+
+      console.log(`${order.user.name} <${order.user.email}>`);
+
+      mailgun()
+        .messages()
+        .send(
+          {
+            from: 'Savore <postmaster@sandbox354a5370f9dc49789738d6c0a93067b5.mailgun.org>',
+            to: `${order.user.name} <${order.user.email}>`,
+            subject: `New order ${order._id}`,
+            html: payOrderEmailTemplate(order),
+          },
+          (error, body) => {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log(body);
+            }
+          }
+        );
       res.send({ message: 'Order Paid', order: updatedOrder });
     } else {
       res.status(404).send({ message: 'Order Not Found' });
