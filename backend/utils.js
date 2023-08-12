f03d98aa02b598f5c5e360f808114d06b254db0b
tranminhchau03 <136145117+tranminhchau03@users.noreply.github.com>
2023-08-12 22:15:47 +0700

    Update utils.js

--- backend/utils.js
+++ backend/utils.js
@@ -98,15 +98,15 @@ export const payOrderEmailTemplate = (order) => {
       <tfoot>
         <tr>
           <td colspan="3">Items Price:</td>
-          <td align="right"> $${order.itemsPrice.toFixed(2)}</td>
+          <td align="right"> ${order.itemsPrice.toFixed(2)}</td>
         </tr>
         <tr>
           <td colspan="3">Shipping Price:</td>
-          <td align="right"> $${order.shippingPrice.toFixed(2)}</td>
+          <td align="right"> ${order.shippingPrice.toFixed(2)}</td>
         </tr>
         <tr>
           <td colspan="3"><strong>Total Price:</strong></td>
-          <td align="right"><strong> $${order.totalPrice.toFixed(
+          <td align="right"><strong> ${order.totalPrice.toFixed(
             2
           )}</strong></td>
         </tr>
@@ -162,7 +162,7 @@ export const deliveringNotificationEmailTemplate = (order) => {
             <td>${item.name}</td>
             <td align="center">${item.quantity}</td>
             <td align="center">${item.size}</td>
-            <td align="right"> $${item.totalPriceProduct.toFixed(2)}</td>
+            <td align="right"> ${item.totalPriceProduct.toFixed(2)}</td>
           </tr>
         `
           )
@@ -171,15 +171,15 @@ export const deliveringNotificationEmailTemplate = (order) => {
       <tfoot>
         <tr>
           <td colspan="3">Items Price:</td>
-          <td align="right"> $${order.itemsPrice.toFixed(2)}</td>
+          <td align="right"> ${order.itemsPrice.toFixed(2)}</td>
         </tr>
         <tr>
           <td colspan="3">Shipping Price:</td>
-          <td align="right"> $${order.shippingPrice.toFixed(2)}</td>
+          <td align="right"> ${order.shippingPrice.toFixed(2)}</td>
         </tr>
         <tr>
           <td colspan="3"><strong>Total Price:</strong></td>
-          <td align="right"><strong> $${order.totalPrice.toFixed(
+          <td align="right"><strong> ${order.totalPrice.toFixed(
             2
           )}</strong></td>
         </tr>
@@ -244,7 +244,7 @@ export const deliveredNotificationEmailTemplate = (order) => {
           <td>${item.name}</td>
           <td align="center">${item.quantity}</td>
           <td align="center">${item.size}</td>
-          <td align="right"> $${item.totalPriceProduct.toFixed(2)}</td>
+          <td align="right"> ${item.totalPriceProduct.toFixed(2)}</td>
         </tr>
       `
         )
@@ -253,15 +253,15 @@ export const deliveredNotificationEmailTemplate = (order) => {
       <tfoot>
         <tr>
           <td colspan="3">Items Price:</td>
-          <td align="right"> $${order.itemsPrice.toFixed(2)}</td>
+          <td align="right"> ${order.itemsPrice.toFixed(2)}</td>
         </tr>
         <tr>
           <td colspan="3">Shipping Price:</td>
-          <td align="right"> $${order.shippingPrice.toFixed(2)}</td>
+          <td align="right"> ${order.shippingPrice.toFixed(2)}</td>
         </tr>
         <tr>
           <td colspan="3"><strong>Total Price:</strong></td>
-          <td align="right"><strong> $${order.totalPrice.toFixed(
+          <td align="right"><strong> ${order.totalPrice.toFixed(
             2
           )}</strong></td>
         </tr>
