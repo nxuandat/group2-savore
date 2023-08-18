cf73b7d7d25b0d772ce2445335ba1bb453be6e67
DinziloZ <121272808+DinziloZ@users.noreply.github.com>
2023-08-18 07:51:12 +0700

    Fix Search Screen

--- frontend/src/screens/SearchScreen.js
+++ frontend/src/screens/SearchScreen.js
@@ -74,7 +74,7 @@ export const ratings = [
 export default function SearchScreen() {
   const navigate = useNavigate();
   const { search } = useLocation();
-  const sp = new URLSearchParams(search); 
+  const sp = new URLSearchParams(search);
   // /search?category=Tea
   const category = sp.get('category') || 'all';
   const query = sp.get('query') || 'all';
@@ -99,7 +99,7 @@ export default function SearchScreen() {
       } catch (err) {
         dispatch({
           type: 'FETCH_FAIL',
-          payload: getError(error),
+          payload: err.message,
         });
       }
     };
@@ -119,16 +119,17 @@ export default function SearchScreen() {
     fetchCategories();
   }, [dispatch]);
 
-  const getFilterUrl = (filter, skipPathname) => {
+  const getFilterUrl = (filter, skipPathname = false) => {
     const filterPage = filter.page || page;
     const filterCategory = filter.category || category;
     const filterQuery = filter.query || query;
     const filterRating = filter.rating || rating;
     const filterPrice = filter.price || price;
     const sortOrder = filter.order || order;
-    return `${
-      skipPathname ? '' : '/search?'
-    }category=${filterCategory}&query=${filterQuery}&price=${filterPrice}&rating=${filterRating}&order=${sortOrder}&page=${filterPage}`;
+
+    const queryString = `category=${filterCategory}&query=${filterQuery}&price=${filterPrice}&rating=${filterRating}&order=${sortOrder}&page=${filterPage}`;
+
+    return `${skipPathname ? '' : '/search?'}${queryString}`;
   };
   return (
     <div>
@@ -269,7 +270,7 @@ export default function SearchScreen() {
                     className="mx-1"
                     to={{
                       pathname: '/search',
-                      seacrh: getFilterUrl({ page: x + 1 }, true),
+                      search: getFilterUrl({ page: x + 1 }, true), // Sửa 'seacrh' thành 'search'
                     }}
                   >
                     <Button
